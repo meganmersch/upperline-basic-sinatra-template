c776401f9889d6b3c25b53db76d6298c55483f97
meganmersch <meganrm8@gmail.com>
2018-08-01 20:21:23 +0000

    first

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,17 @@ class ApplicationController < Sinatra::Base
     return erb :index
   end
 end
+def volunteer_queens(volunteer_value)
+  
+  if volunteer_value <=11
+    return "Cooking/Food"
+  elsif volunteer_value <=14
+    return "Helping People"
+  elsif volunteer_value <=17
+    return "Environment/Animals"
+  elsif volunteer_value <=20
+    return "Sports"
+    
+  end
+end
+volunteer_queens ==14
